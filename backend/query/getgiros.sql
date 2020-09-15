12a394dbdb8385c942cc45cc75376714c6ec3ea0
Leo Borquez <leoborquezg@protonmail.com>
2020-09-15 00:03:36 -0300

    🐛 FIX: query getgiros

--- backend/query/getgiros.sql
+++ backend/query/getgiros.sql
@@ -6,23 +6,30 @@
     @FechaIni : Se debe ingresar en formato yyy-MM-dd    
     @FechaFin : Se debe ingresar en formato yyy-MM-dd    
 */
+/* 
+  Parámetros utilizados para obtener los giros
+    @CodigoProducto 
+        37 (Giro Cuenta 2)
+        38 (Giro APV)
+    @FechaIni : Se debe ingresar en formato yyy-MM-dd    
+    @FechaFin : Se debe ingresar en formato yyy-MM-dd    
+*/
 
 DECLARE @FechaIni varchar(10), @FechaFin varchar(10), @CodigoProducto int;
 SET @FechaIni = N'2020-01-01';
 SET @FechaFin = N'2020-09-07';
-SET @CodigoProducto = 38;
+SET @CodigoProducto = 37;
 
 
 
 
 WITH q AS (
 	SELECT
-		t.TRX_FECHA_TRANSACCION_FC AS 'Fecha transacción',
-		t.TRX_FOLIO_TRANSACCION_NN AS 'Nro Transacción',
-		t.TRX_MO,
+		t.TRX_FECHA_TRANSACCION_FC AS 'fecha',
+		t.TRX_FOLIO_TRANSACCION_NN AS 'transaccion',
 		RTRIM(CONVERT(char,
-				a.ACC_RUT_CLIENTE_NN)) + '-' + a.ACC_DV_RUT_CLIENTE_CR AS 'Rut Cliente',
-		a.ACC_HOST_IP AS 'IP Cliente',
+				a.ACC_RUT_CLIENTE_NN)) + '-' + a.ACC_DV_RUT_CLIENTE_CR AS 'rutCliente',
+		a.ACC_HOST_IP AS 'ipCliente',
 		CASE t.TRX_ESTADO_TA
 		WHEN 'ACE' THEN
 			'OK'
@@ -31,11 +38,11 @@ WITH q AS (
 		ELSE
 			''
 		END AS Estado,
-		'' AS 'Error'
+		'' AS 'errorDetalle'
 	FROM
 		TRANSACCION_PASOS t
 		INNER JOIN ACCION a ON a.ACC_ACCION_ID = t.TRX_ACCION_ID
-			AND a.ACC_RUT_CLIENTE_NN = 15019701
+			--AND a.ACC_RUT_CLIENTE_NN = 15019701
 			AND a.ACC_OPERACION_TA = t.TRX_OPERACION_TA
 			AND a.ACC_SECUENCIA_ACCION_ID = t.TRX_SECUENCIA_ACCION
 			AND t.TRX_FECHA_TRANSACCION_FC >= @FechaIni
@@ -48,4 +55,4 @@ SELECT
 FROM
 	q
 ORDER BY
-	q. [Fecha transacción] DESC
+	q.[fecha] DESC
